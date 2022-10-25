31d13892ed333f4e7a8f1b1c584c712e2036a011
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-10-25 20:34:32 +0700

    nice

--- whileLoops.js
+++ whileLoops.js
@@ -12,25 +12,7 @@ timeToMixJuice('Tropical Island');
 
 timeToMixJuice('Berries & Lime');
 // => 2.5
-2. Replenish the lime wedge supply
-A lot of Li Mei's creations include lime wedges, either as an ingredient or as part of the decoration. So when she starts her shift in the morning she needs to make sure the bin of lime wedges is full for the day ahead.
 
-Implement the function limesToCut which takes the number of lime wedges Li Mei needs to cut and an array representing the supply of whole limes she has at hand. She can get 6 wedges from a 'small' lime, 8 wedges from a 'medium' lime and 10 from a 'large' lime. She always cuts the limes in the order in which they appear in the list, starting with the first item. She keeps going until she reached the number of wedges that she needs or until she runs out of limes.
-
-Li Mei would like to know in advance how many limes she needs to cut. The limesToCut function should return the number of limes to cut.
-
-limesToCut(25, ['small', 'small', 'large', 'medium', 'small']);
-// => 4
-3. Finish up the shift
-Li Mei always works until 3pm. Then her employee Dmitry takes over. There are often drinks that have been ordered but are not prepared yet when Li Mei's shift ends. Dmitry will then prepare the remaining juices.
-
-To make the hand-over easier, implement a function remainingOrders which takes the number of minutes left in Li Mei's shift and an array of juices that have been ordered but not prepared yet. The function should return the orders that Li Mei cannot start preparing before the end of her workday.
-
-The time left in the shift will always be greater than 0. Furthermore, the orders are prepared in the order in which they appear in the array. If Li Mei starts to mix a certain juice, she will always finish it even if she has to work a bit longer. If there are no remaining orders left that Dmitry needs to take care of, an empty array should be returned.
-
-remainingOrders(5, ['Energizer', 'All or Nothing', 'Green Garden']);
-// => ['Green Garden']
-*/
 
 const prompt = require("prompt-sync")();
 let name = prompt('what type u want? : ');
@@ -63,10 +45,60 @@ switch (name) {
 
 timeToMixJuice(name)
 
-
-/*
 const prompt = require("prompt-sync")();
 
 let a = prompt("enter a number: ")
 console.log(a)
+*/
+
+/*
+2. Replenish the lime wedge supply
+A lot of Li Mei's creations include lime wedges, either as an ingredient or as part of the decoration. So when she starts her shift in the morning she needs to make sure the bin of lime wedges is full for the day ahead.
+
+Implement the function limesToCut which takes the number of lime wedges Li Mei needs to cut and an array representing the supply of whole limes she has at hand. She can get 6 wedges from a 'small' lime, 8 wedges from a 'medium' lime and 10 from a 'large' lime. She always cuts the limes in the order in which they appear in the list, starting with the first item. She keeps going until she reached the number of wedges that she needs or until she runs out of limes.
+
+Li Mei would like to know in advance how many limes she needs to cut. The limesToCut function should return the number of limes to cut.
+
+limesToCut(25, ['small', 'small', 'large', 'medium', 'small']);
+// => 4
+*/
+
+//know how many slices she needs
+//know what limes she has
+//know total slices of the limes she has
+//know the total of limes she needs
+//----///
+
+//know how many slices she needs
+//see the lime array, identify each one then add them all together 
+let limeList = ['small', 'small', 'large', 'medium', 'small'];
+let x=0;
+for(let i=0; i<limeList.length; i++){
+    let slices=0;
+    switch(limeList){
+        case 'small':
+            slices=6;
+            break;
+        case 'medium':
+            slices=8;
+            break;
+        case 'large':
+            slices=10;
+            break;
+    }
+    x=x+slices;
+}
+console.log(x);
+
+
+/*
+3. Finish up the shift
+Li Mei always works until 3pm. Then her employee Dmitry takes over. There are often drinks that have been ordered but are not prepared yet when Li Mei's shift ends. Dmitry will then prepare the remaining juices.
+
+To make the hand-over easier, implement a function remainingOrders which takes the number of minutes left in Li Mei's shift and an array of juices that have been ordered but not prepared yet. The function should return the orders that Li Mei cannot start preparing before the end of her workday.
+
+The time left in the shift will always be greater than 0. Furthermore, the orders are prepared in the order in which they appear in the array. If Li Mei starts to mix a certain juice, she will always finish it even if she has to work a bit longer. If there are no remaining orders left that Dmitry needs to take care of, an empty array should be returned.
+
+remainingOrders(5, ['Energizer', 'All or Nothing', 'Green Garden']);
+// => ['Green Garden']
 */
